2c4549eadc460b429f5fb02d99e419c4f66804b6
Md. Shahriar Haque <81591350+xenoncolt@users.noreply.github.com>
2024-03-08 06:43:48 +0600

    Add abstract classes and methods

---
+++ ab/exExtendsAbstract.java
@@ -0,0 +1,11 @@
+package ab;
+
+import custom.out.output;
+
+public abstract class exExtendsAbstract extends extendsAbstract {
+    public void abUni() {
+        output.bolo("AIUB");
+    }
+
+    public abstract int multiple(int text,int text2);
+}
